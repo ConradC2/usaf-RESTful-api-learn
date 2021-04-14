3cc34d23abe79ca706de2232d01878f9c6e61bb2
Curtis Conrad <conradc5749@yahoo.com64824279+ConradC2@users.noreply.github.com>
2021-04-14 16:59:20 -0400

    added student grade

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const students = require('./students.json')
 const port = 3000;
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 app.get('/students', (request, response) => {
     if (request.query.name){
@@ -12,7 +17,25 @@ app.get('/students', (request, response) => {
 })
 
 app.get('/students/:studentId', (request, response) => {
-    response.send(students.filter(student => student.id == request.params.studentId));
+    response.send(students.find(student => student.id == request.params.studentId));
 });
+
+app.get('/grades/:studentId', (request, response) => {
+    response.send(students.find(student => student.id == request.params.studentId).grades);
+});
+
+app.post('/grades', (request, response) => {
+  console.log(request.body);
+    if (request.body.studentId && request.body.grade){
+        const { studentId, grade } = request.body;
+        console.log(studentId, grade);
+        let studentUpdated = students.find(student.id == studentId);
+        studentUpdated.grades.push(grade);
+    response.send(`You added a grade ${grade} to ${studentUpdated.name}`);
+    } else {
+        response.send("You must include a studentID and Grade!");
+    }
+});
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
